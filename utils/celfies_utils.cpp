b892911f3681b8fcfb3390184b40e81fb22e437c
John Doe <johndoe@example.com>
2021-11-17 00:55:20 +0000

    added first utilty function - counting celfies

---
+++ utils/celfies_utils.cpp
@@ -0,0 +1,27 @@
+#include<iostream> 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int len_celfies(string celfies)
+{
+/**
+ * Returns the number of symbols in a given SELFIES string.
+ * param selfies: a SELFIES string.
+ * return: the symbol length of the SELFIES string.
+ *
+ * Example:
+ * len_selfies("[C][=C][F].[C]")
+ * >>> 5
+ */
+	
+	return count(celfies.begin(), celfies.end(), '[') + count(celfies.begin(), celfies.end(), '.');
+}
+
+
+int main()
+{
+	len_celfies("[C] [=C].[C]");
+
+	return 0; 
+}
