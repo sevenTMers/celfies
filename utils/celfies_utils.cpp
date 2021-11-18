7d45a336558e9107c96cebfe8f6dd2a3fdbf2020
John Doe <johndoe@example.com>
2021-11-18 04:23:06 +0000

    mostly finished celfies_utils.cpp - need to fix regex to include brackets in result

--- utils/celfies_utils.cpp
+++ utils/celfies_utils.cpp
@@ -1,6 +1,8 @@
 #include<iostream> 
 #include<algorithm>
-
+#include<vector> 
+#include<regex>
+#include<iterator>
 using namespace std;
 
 int len_celfies(string celfies)
@@ -18,10 +20,31 @@ int len_celfies(string celfies)
 	return count(celfies.begin(), celfies.end(), '[') + count(celfies.begin(), celfies.end(), '.');
 }
 
+void split_celfies(string celfies)
+{
+/**
+ * Tokenizes a SELFIES string into its individual symbols.
+ *
+ * :param selfies: a SELFIES string.
+ * :return: the symbols of the SELFIES string one-by-one with order preserved.
+ *
+ * :Example:
+ * split_selfies("[C][=C][F].[C]"))
+ * >>> ['[C]', '[=C]', '[F]', '.', '[C]']
+ */ 
+
+std::regex smiles_re("\[[^\\]]"); 
+std::copy( std::sregex_token_iterator(celfies.begin(), celfies.end(), smiles_re, -1),
+      std::sregex_token_iterator(),
+      std::ostream_iterator<std::string>(std::cout, "\n"));
+}
+
+// get_alphabet_from_selfies to be implemented later...maybe 
+
 
 int main()
 {
-	len_celfies("[C] [=C].[C]");
+	split_celfies("[C][=C][F].[C]");
 
 	return 0; 
 }
