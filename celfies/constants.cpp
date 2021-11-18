8938208387a2975f04503d9e2ff1cfcad367b335
John Doe <johndoe@example.com>
2021-11-18 22:53:15 +0000

    started constants file

---
+++ celfies/constants.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+// reconsider container type later
+std::vector<string> Elements = {
+	  "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
+    "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr",
+    "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br",
+    "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd",
+    "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf",
+    "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi",
+    "Po", "At", "Rn", "Fr", "Ra", "Rf", "Db", "Sg", "Bh", "Hs", "Mt",
+    "Ds", "Rg", "Cn", "Fl", "Lv", "La", "Ce", "Pr", "Nd", "Pm", "Sm",
+    "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Ac", "Th",
+    "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md",
+    "No", "Lr"
+};
+
+std::vector<string> OrganicSubset = {"B", "C", "N", "O", "S", "P", "F", "Cl", "Br", "I"};
+
+//placeholder code for map
+//ToDo
+std::map<string, int> AromaticValences = {
+    { 'B', (3,) },
+    { 'Al', (3,) },
+    { 'C',  (4,)},
+		{ 'Si', (4,) },
+		{ 'N', (3, 5) },
+		{ 'P', (3, 5) },
+		{ 'As', (3, 5) },
+		{ 'O', (2,4) },
+		{ 'S', (2,4) },
+		{ 'Se', (2,4) },
+		{ 'Te', (2,4)}
+};
+
+
