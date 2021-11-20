26120218d2e574e46c571e0205d2c10557d95fe6
John Doe <johndoe@example.com>
2021-11-20 14:52:21 +0000

    updated constants

--- celfies/constants.cpp
+++ celfies/constants.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <map>
 
 // reconsider container type later
-std::vector<string> Elements = {
-	  "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
-    "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr",
-    "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br",
-    "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd",
-    "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf",
-    "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi",
-    "Po", "At", "Rn", "Fr", "Ra", "Rf", "Db", "Sg", "Bh", "Hs", "Mt",
-    "Ds", "Rg", "Cn", "Fl", "Lv", "La", "Ce", "Pr", "Nd", "Pm", "Sm",
-    "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Ac", "Th",
-    "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md",
-    "No", "Lr"
+string elements[118] = {
+   "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
+   "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr",
+   "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br",
+   "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd",
+   "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "Hf",
+   "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi",
+   "Po", "At", "Rn", "Fr", "Ra", "Rf", "Db", "Sg", "Bh", "Hs", "Mt",
+   "Ds", "Rg", "Cn", "Fl", "Lv", "La", "Ce", "Pr", "Nd", "Pm", "Sm",
+   "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Ac", "Th",
+   "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md",
+   "No", "Lr"
 };
 
-std::vector<string> OrganicSubset = {"B", "C", "N", "O", "S", "P", "F", "Cl", "Br", "I"};
+string organicSubset[10] = {"B", "C", "N", "O", "S", "P", "F", "Cl", "Br", "I"};
 
 //placeholder code for map
 //ToDo
 std::map<string, int> AromaticValences = {
-    { 'B', (3,) },
-    { 'Al', (3,) },
-    { 'C',  (4,)},
-		{ 'Si', (4,) },
-		{ 'N', (3, 5) },
-		{ 'P', (3, 5) },
-		{ 'As', (3, 5) },
-		{ 'O', (2,4) },
-		{ 'S', (2,4) },
-		{ 'Se', (2,4) },
-		{ 'Te', (2,4)}
+   { 'B', (3,) },
+   { 'Al', (3,) },
+   { 'C',  (4,)},
+   { 'Si', (4,) },
+   { 'N', (3, 5) },
+   { 'P', (3, 5) },
+   { 'As', (3, 5) },
+   { 'O', (2,4) },
+   { 'S', (2,4) },
+   { 'Se', (2,4) },
+   { 'Te', (2,4)}
 };
 
+// aromaticSubset
 
+string indexAlphabet[16] = {
+   "[C]", "[Ring1]", "[Ring2]",
+   "[Branch1]", "[=Branch1]", "[#Branch1]",
+   "[Branch2]", "[=Branch2]", "[#Branch2]",
+   "[O]", "[N]", "[=N]", "[=C]", "[#C]", "[S]", "[P]"
+}
+
+//indexCode
